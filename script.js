514b0cba9b952c76dd420ac986befa6ba43ad1e9
Paris Apostolopoulos <apostolo@us5mcl-c02dd2uyml7j.ttg.global>
2021-01-24 15:48:28 -0800

    added first api call

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-var request = new XMLHttpRequest();
+let metadataRequest = new XMLHttpRequest();
 
-request.open('GET', 'https://hplussport.com/api/products');
+metadataRequest.open('GET', 'http://api.tomtom.com/map/1/glyph/20.0.0-8/metadata.json?key=aF6RzykjXBBSEAaHf396gG3qq5YRGerX');
 
-request.onload = function() {
-	var response = request.response;
-	console.log(response);
-	console.log(typeof response);
-	var jsonData = JSON.parse(response);
-	console.log(jsonData);
-	console.log(typeof jsonData);
+metadataRequest.onload = function() {
+    let response = metadataRequest.response;
+	let parsedData = JSON.parse(response).content;
+    console.log(parsedData);
+
+    //Create Header and unordered list elements
+    let metaHeader = document.createElement("h1");
+    metaHeader.innerHTML = "Metadata API";
+    document.body.appendChild(metaHeader);
+	let unorderedList = document.createElement("ul");
+	document.body.appendChild(unorderedList);
+
+    //Iterate through the JSON data to display the information
+	for(item in parsedData){
+		let metaElement = document.createElement('li');
+		metaElement.innerHTML = `Name: ${parsedData[item].name} / Type: ${parsedData[item].type}`;
+        unorderedList.appendChild(metaElement);
+
+    }
+    
+    let metaHR = document.createElement("hr");
+    document.body.appendChild(metaHR);
 };
 
-request.send();
+metadataRequest.send();
